1a3a534c1c4f412f57e5046e9bc6062db2ddb85c
Dancan254 <dancanian25@gmail.com>
2025-07-06 19:44:33 +0300

    Add AuthResponse class for handling authentication responses

---
+++ src/main/java/com/javaguy/superbaseauthdemo/model/AuthResponse.java
@@ -0,0 +1,24 @@
+package com.javaguy.superbaseauthdemo.model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class AuthResponse {
+    @JsonProperty("access_token")
+    private String accessToken;
+    @JsonProperty("refresh_token")
+    private String refreshToken;
+    @JsonProperty("token_type")
+    private String tokenType;
+    @JsonProperty("expires_in")
+    private Integer expiresIn;
+
+    private User user;
+}
