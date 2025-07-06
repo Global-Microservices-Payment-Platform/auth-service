845a4bf3df403dfcae3db1c81ae53cf099c647f5
Dancan254 <dancanian25@gmail.com>
2025-07-06 19:45:38 +0300

    Add LoginRequest class for handling user login requests with validation

---
+++ src/main/java/com/javaguy/superbaseauthdemo/model/LoginRequest.java
@@ -0,0 +1,24 @@
+package com.javaguy.superbaseauthdemo.model;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class LoginRequest {
+
+    @Email(message = "Please provide a valid email address")
+    @NotBlank(message = "Email is required")
+    private String email;
+
+    @NotBlank(message = "Password is required")
+    @Size(min = 8, message = "Password must be at least 8 characters long")
+    private String password;
+}
